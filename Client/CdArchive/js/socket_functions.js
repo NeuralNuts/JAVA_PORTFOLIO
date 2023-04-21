9ed6270d829d4d3cf8bf219387788d2793e38665
NeuralNuts <wilsonzakk8@gmail.com>
2023-04-21 21:55:19 +1000

    misc

--- Client/CdArchive/js/socket_functions.js
+++ Client/CdArchive/js/socket_functions.js
@@ -1,24 +1,25 @@
- // Connect to the server using socket.io
- const socket = io("http://localhost:3000");
- console.log(socket)
+// Connect to the server using socket.io
+const socket = io("http://localhost:3000");
+console.log(socket)
 
- // Listen for incoming messages from the server
- socket.on('message', (data) => {
-     // Add the message to the chat log
-     const chatLog = document.getElementById('chat-log');
-     const newMessage = document.getElementById('textarea-input');
-     newMessage.textContent = data;
-     //chatLog.appendChild(newMessage);
- });
+// Listen for incoming messages from the server
+socket.on('message', (data) => {
+    // Add the message to the chat log
+    const chatLog = document.getElementById('log-div');
+    const newMessage = document.createElement('input');
+    newMessage.value = data;
 
- window.addEventListener('load', () => {
-     const form = document.getElementById('message-form');
-     form.addEventListener('submit', (event) => {
-         event.preventDefault();
-         const messageInput = document.getElementById('message-input');
-         const message = messageInput.value;
-         socket.emit('message', message);
-         messageInput.value = '';
-     });
- })
+    chatLog.appendChild(newMessage);
+});
+
+window.addEventListener('load', () => {
+    const form = document.getElementById('message-form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const messageInput = document.getElementById('message-input');
+        const message = messageInput.value;
+        socket.emit('message', message);
+        messageInput.value = '';
+    });
+})
  // Handle form submission to send messages
