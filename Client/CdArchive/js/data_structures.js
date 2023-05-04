576532b7de5bdd4d5d470f3b35ad0a6f6b675d95
NeuralNuts <wilsonzakk8@gmail.com>
2023-05-05 02:33:01 +1000

    linked list complete

---
+++ Client/CdArchive/js/data_structures.js
@@ -0,0 +1,248 @@
+class Node {
+  constructor(key, name) {
+    this.key = key;
+    this.name = name;
+    this.leftChild = null;
+    this.rightChild = null;
+  }
+  toString() {
+    //return `${this.name}has the key${this.key}`;
+    //return this.name + " has the key " + this.key + "\nLeft Child: " + this.leftChild + "\nRight Child: " + this.rightChild + "\n";
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  addNode(key, name) {
+    const newNode = new Node(key, name);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let focusNode = this.root;
+      let parent;
+      while (true) {
+        parent = focusNode;
+        if (key < focusNode.key) {
+          focusNode = focusNode.leftChild;
+          if (focusNode === null) {
+            parent.leftChild = newNode;
+            return;
+          }
+        } else {
+          focusNode = focusNode.rightChild;
+          if (focusNode === null) {
+            parent.rightChild = newNode;
+            return;
+          }
+        }
+      }
+    }
+  }
+  inOrderTraverseTree(focusNode) {
+    if (focusNode !== null) {
+      this.inOrderTraverseTree(focusNode.leftChild);
+      console.log(focusNode.toString());
+      this.inOrderTraverseTree(focusNode.rightChild);
+    }
+  }
+  preorderTraverseTree(focusNode) {
+    if (focusNode !== null) {
+      console.log(focusNode.toString());
+      this.preorderTraverseTree(focusNode.leftChild);
+      this.preorderTraverseTree(focusNode.rightChild);
+    }
+  }
+  postOrderTraverseTree(focusNode) {
+    if (focusNode !== null) {
+      this.postOrderTraverseTree(focusNode.leftChild);
+      this.postOrderTraverseTree(focusNode.rightChild);
+      console.log(focusNode.toString());
+    }
+  }
+  findNode(key) {
+    let focusNode = this.root;
+    while (focusNode.key !== key) {
+      if (key < focusNode.key) {
+        focusNode = focusNode.leftChild;
+      } else {
+        focusNode = focusNode.rightChild;
+      }
+      if (focusNode === null) {
+        return null;
+      }
+    }
+    return focusNode;
+  }
+}
+
+function loadDataTree() {
+
+  get_data = `SELECT * FROM archive`;
+
+  db.all(get_data, [], (err, rows) => {
+
+    if (err) return console.error(err.message);
+
+    for (let row of rows) {
+
+      const theTree = new BinaryTree();
+      theTree.addNode(40, row.title);
+      theTree.addNode(30, row.title);
+      theTree.addNode(20, row.title);
+      theTree.addNode(70, row.title);
+      theTree.addNode(90, row.title);
+
+
+      // Different ways to traverse binary trees
+      //theTree.inOrderTraverseTree(theTree.root);
+      //theTree.preorderTraverseTree(theTree.root);
+      //theTree.postOrderTraverseTree(theTree.root);
+
+      // Find the node with key 75
+      // console.log("\nTest Node with the key 75");
+      //console.log(theTree);
+      m = JSON.stringify(theTree)
+      console.log(m)
+    }
+  })
+};
+
+class Node_2 {
+  constructor(value) {
+      this.value = value;
+      this.next = null;
+      this.previous = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+      this.head = {
+          value: value,
+          next: null,
+          previous: null
+      };
+      this.length = 1;
+      this.tail = this.head;
+  }
+
+  printList() {
+      let array = [];
+      let currentList = this.head;
+      while (currentList !== null) {
+          array.push(currentList.value);
+          currentList = currentList.next;
+      }
+
+      //array.join('<-->');
+      return this;
+  }
+
+  // Insert node at end of the list
+  append(value) {
+      let newNode = new Node_2(value);
+
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+
+      this.length++;
+      this.printList();
+  }
+
+  // Insert node at the start of the list
+  prepend(value) {
+      let newNode = new Node_2(value);
+
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+
+      this.length++;
+      this.printList();
+  }
+
+  // Insert node at a given index
+  insert(index, value) {
+      if (!Number.isInteger(index) || index < 0 || index > this.length + 1) {
+          console.log(`Invalid index. Current length is ${this.length}.`);
+          return this;
+      }
+
+      // If index is 0, prepend
+      if (index === 0) {
+          this.prepend(value);
+          return this;
+      }
+
+      // If index is equal to this.length, append
+      if (index === this.length) {
+          this.append(value);
+          return this;
+      }
+
+      // Reach the node at that index
+      let newNode = new Node_2(value);
+      let previousNode = this.head;
+
+      for (let k = 0; k < index - 1; k++) {
+          previousNode = previousNode.next;
+      }
+
+      let nextNode = previousNode.next;
+
+      newNode.next = nextNode;
+      previousNode.next = newNode;
+      newNode.previous = previousNode;
+      nextNode.previous = newNode;
+
+      this.length++;
+      this.printList();
+  }
+
+  // Remove a node
+  remove(index) {
+      if (!Number.isInteger(index) || index < 0 || index > this.length) {
+          console.log(`Invalid index. Current length is ${this.length}.`);
+          return this;
+      }
+
+      // Remove head
+      if (index === 0) {
+          this.head = this.head.next;
+          this.head.previous = null;
+
+          this.length--;
+          this.printList();
+          return this;
+      }
+
+      // Remove tail
+      if (index === this.length - 1) {
+          this.tail = this.tail.previous;
+          this.tail.next = null;
+
+          this.length--;
+          this.printList();
+          return this;
+      }
+
+      // Remove node at an index
+      let previousNode = this.head;
+
+      for (let k = 0; k < index - 1; k++) {
+          previousNode = previousNode.next;
+      }
+      let deleteNode = previousNode.next;
+      let nextNode = deleteNode.next;
+
+      previousNode.next = nextNode;
+      nextNode.previous = previousNode;
+
+      this.length--;
+      this.printList();
+      return this;
+  }
+}
--- Client/CdArchive/js/socket_functions.js
+++ Client/CdArchive/js/socket_functions.js
@@ -7,144 +7,6 @@ var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getD
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date + time;
 
-class Node_2 {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-        this.previous = null;
-    }
-}
-
-class DoublyLinkedList {
-    constructor(value) {
-        this.head = {
-            value: value,
-            next: null,
-            previous: null
-        };
-        this.length = 1;
-        this.tail = this.head;
-    }
-
-    printList() {
-        let array = [];
-        let currentList = this.head;
-        while (currentList !== null) {
-            array.push(currentList.value);
-            currentList = currentList.next;
-        }
-
-        //array.join('<-->');
-        return this;
-    }
-
-    // Insert node at end of the list
-    append(value) {
-        let newNode = new Node_2(value);
-
-        this.tail.next = newNode;
-        newNode.previous = this.tail;
-        this.tail = newNode;
-
-        this.length++;
-        this.printList();
-    }
-
-    // Insert node at the start of the list
-    prepend(value) {
-        let newNode = new Node_2(value);
-
-        newNode.next = this.head;
-        this.head.previous = newNode;
-        this.head = newNode;
-
-        this.length++;
-        this.printList();
-    }
-
-    // Insert node at a given index
-    insert(index, value) {
-        if (!Number.isInteger(index) || index < 0 || index > this.length + 1) {
-            console.log(`Invalid index. Current length is ${this.length}.`);
-            return this;
-        }
-
-        // If index is 0, prepend
-        if (index === 0) {
-            this.prepend(value);
-            return this;
-        }
-
-        // If index is equal to this.length, append
-        if (index === this.length) {
-            this.append(value);
-            return this;
-        }
-
-        // Reach the node at that index
-        let newNode = new Node_2(value);
-        let previousNode = this.head;
-
-        for (let k = 0; k < index - 1; k++) {
-            previousNode = previousNode.next;
-        }
-
-        let nextNode = previousNode.next;
-
-        newNode.next = nextNode;
-        previousNode.next = newNode;
-        newNode.previous = previousNode;
-        nextNode.previous = newNode;
-
-        this.length++;
-        this.printList();
-    }
-
-    // Remove a node
-    remove(index) {
-        if (!Number.isInteger(index) || index < 0 || index > this.length) {
-            console.log(`Invalid index. Current length is ${this.length}.`);
-            return this;
-        }
-
-        // Remove head
-        if (index === 0) {
-            this.head = this.head.next;
-            this.head.previous = null;
-
-            this.length--;
-            this.printList();
-            return this;
-        }
-
-        // Remove tail
-        if (index === this.length - 1) {
-            this.tail = this.tail.previous;
-            this.tail.next = null;
-
-            this.length--;
-            this.printList();
-            return this;
-        }
-
-        // Remove node at an index
-        let previousNode = this.head;
-
-        for (let k = 0; k < index - 1; k++) {
-            previousNode = previousNode.next;
-        }
-        let deleteNode = previousNode.next;
-        let nextNode = deleteNode.next;
-
-        previousNode.next = nextNode;
-        nextNode.previous = previousNode;
-
-        this.length--;
-        this.printList();
-        return this;
-    }
-}
-
 // Listen for incoming messages from the server
 socket.on('message', (section_data) => {
     //Add the message to the chat log
@@ -153,46 +15,32 @@ socket.on('message', (section_data) => {
     chatLog.innerHTML = newMessage
     const sectionInput = document.getElementById('pro-log-input')
     sectionInput.value = section_data;
-    chatLog.appendChild(sectionInput);
-    console.log(section_data)
 
     socket.on('message_2', (barcode_data) => {
         //Add the message to the chat log
         const chatLog = document.getElementById('log-div');
-        const newMessage = document.createElement('input');
-        newMessage.value = barcode_data;
-        chatLog.appendChild(newMessage);
         const barCodeInput = document.getElementById("bar-code-input")
         barCodeInput.value = barcode_data
 
-        results = `Date/Time: ${dateTime} | ${section_data} | ${barcode_data}`
-        var myDoublyList = new DoublyLinkedList(results);
-        console.log(myDoublyList.printList())
-        sectionInput.value = JSON.stringify(myDoublyList.printList().head.value)
+        socket.on('message_x', (x_data) => {
+            //Add the message to the chat log
+            const chatLog = document.getElementById('log-div');
+            const newMessage = document.createElement('input');
+            newMessage.value = x_data;
+            const xInput = document.getElementById("x-input")
+            xInput.value = x_data
+
+            socket.on('message_y', (y_data) => {
+                const yInput = document.getElementById("y-input")
+                yInput.value = y_data
+    
+                results = `Date/Time: ${dateTime} | ${section_data} | ${barcode_data} | ${x_data} | ${y_data} | ADD-FROM-ROBOT`
+                var myDoublyList = new DoublyLinkedList(results);
+                sectionInput.value = JSON.stringify(myDoublyList.printList().head.value)
+                chatLog.appendChild(sectionInput);
+            });
+        });
     });
-});
-
-
-
-socket.on('message_x', (data) => {
-    //Add the message to the chat log
-    const chatLog = document.getElementById('log-div');
-    const newMessage = document.createElement('input');
-    newMessage.value = data;
-    const xInput = document.getElementById("x-input")
-    xInput.value = data
-});
-
-socket.on('message_y', (data) => {
-    // Add the message to the chat log
-    // const chatLog = document.getElementById('log-div');
-    // const newMessage = document.createElement('input');
-    // newMessage.value = data;
-
-    // chatLog.appendChild(newMessage);
-
-    const yInput = document.getElementById("y-input")
-    yInput.value = data
 });
 
 window.addEventListener('load', () => {
@@ -200,12 +48,6 @@ window.addEventListener('load', () => {
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        // const sectionInput = document.getElementById('title-input');
-        // const message = sectionInput.value;
-
-        // document.getElementById('').innerHTML = "";
-        //get_data = `SELECT * FROM archive`;
-
         var source = document.getElementById('my-table');
         var destination = document.getElementById('my-table-2');
         var copy = source.cloneNode(true);
@@ -213,4 +55,3 @@ window.addEventListener('load', () => {
         console.log(destination)
     });
 })
-//console.log(myDoublyList.printList())
