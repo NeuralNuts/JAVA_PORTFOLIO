657af3d4fe7bb667949ef96fbd0a0bd167f26bc1
NeuralNuts <wilsonzakk8@gmail.com>
2023-05-05 04:03:02 +1000

    misc

--- Client/CdArchive/js/data_structures.js
+++ Client/CdArchive/js/data_structures.js
@@ -79,170 +79,170 @@ class BinaryTree {
 
 function loadDataTree() {
 
-  get_data = `SELECT * FROM archive`;
+  var cells = table.getElementsByTagName('td');
 
-  db.all(get_data, [], (err, rows) => {
+  for (var i = 0; i < cells.length; i++) {
+    // Take each cell
+    var cell = cells[i];
+    console.log(cell)
+    arr = [cells[1].innerHTML]
+    const theTree = new BinaryTree();
+    theTree.addNode(40, arr);
+    // theTree.addNode(40, cells[2].innerHTML);
+    // theTree.addNode(40, cells[3].innerHTML);
+    // theTree.addNode(40, cells[4].innerHTML);
+    // theTree.addNode(40, cells[5].innerHTML);
+    // theTree.addNode(40, cells[6].innerHTML);
 
-    if (err) return console.error(err.message);
+    // Different ways to traverse binary trees
+    //theTree.inOrderTraverseTree(theTree.root);
+    //theTree.preorderTraverseTree(theTree.root);
+    //theTree.postOrderTraverseTree(theTree.root);
 
-    for (let row of rows) {
+    // Find the node with key 75
+    // console.log("\nTest Node with the key 75");
+    //console.log(theTree);
 
-      const theTree = new BinaryTree();
-      theTree.addNode(40, row.title);
-      theTree.addNode(30, row.title);
-      theTree.addNode(20, row.title);
-      theTree.addNode(70, row.title);
-      theTree.addNode(90, row.title);
-
-
-      // Different ways to traverse binary trees
-      //theTree.inOrderTraverseTree(theTree.root);
-      //theTree.preorderTraverseTree(theTree.root);
-      //theTree.postOrderTraverseTree(theTree.root);
-
-      // Find the node with key 75
-      // console.log("\nTest Node with the key 75");
-      //console.log(theTree);
-      m = JSON.stringify(theTree)
-      console.log(m)
-    }
-  })
+    var myArray = `${theTree.inOrderTraverseTree(theTree.root)}`
+    document.getElementById("area-id").innerHTML = JSON.stringify(theTree) //JSON.stringify(myArray) 
+    //console.log(theTree.root)
+  }
 };
 
 class Node_2 {
   constructor(value) {
-      this.value = value;
-      this.next = null;
-      this.previous = null;
+    this.value = value;
+    this.next = null;
+    this.previous = null;
   }
 }
 
 class DoublyLinkedList {
   constructor(value) {
-      this.head = {
-          value: value,
-          next: null,
-          previous: null
-      };
-      this.length = 1;
-      this.tail = this.head;
+    this.head = {
+      value: value,
+      next: null,
+      previous: null
+    };
+    this.length = 1;
+    this.tail = this.head;
   }
 
   printList() {
-      let array = [];
-      let currentList = this.head;
-      while (currentList !== null) {
-          array.push(currentList.value);
-          currentList = currentList.next;
-      }
+    let array = [];
+    let currentList = this.head;
+    while (currentList !== null) {
+      array.push(currentList.value);
+      currentList = currentList.next;
+    }
 
-      array.join('<-->');
-      return this;
+    array.join('<-->');
+    return this;
   }
 
   // Insert node at end of the list
   append(value) {
-      let newNode = new Node_2(value);
+    let newNode = new Node_2(value);
 
-      this.tail.next = newNode;
-      newNode.previous = this.tail;
-      this.tail = newNode;
+    this.tail.next = newNode;
+    newNode.previous = this.tail;
+    this.tail = newNode;
 
-      this.length++;
-      this.printList();
+    this.length++;
+    this.printList();
   }
 
   // Insert node at the start of the list
   prepend(value) {
-      let newNode = new Node_2(value);
+    let newNode = new Node_2(value);
 
-      newNode.next = this.head;
-      this.head.previous = newNode;
-      this.head = newNode;
+    newNode.next = this.head;
+    this.head.previous = newNode;
+    this.head = newNode;
 
-      this.length++;
-      this.printList();
+    this.length++;
+    this.printList();
   }
 
   // Insert node at a given index
   insert(index, value) {
-      if (!Number.isInteger(index) || index < 0 || index > this.length + 1) {
-          console.log(`Invalid index. Current length is ${this.length}.`);
-          return this;
-      }
+    if (!Number.isInteger(index) || index < 0 || index > this.length + 1) {
+      console.log(`Invalid index. Current length is ${this.length}.`);
+      return this;
+    }
 
-      // If index is 0, prepend
-      if (index === 0) {
-          this.prepend(value);
-          return this;
-      }
+    // If index is 0, prepend
+    if (index === 0) {
+      this.prepend(value);
+      return this;
+    }
 
-      // If index is equal to this.length, append
-      if (index === this.length) {
-          this.append(value);
-          return this;
-      }
+    // If index is equal to this.length, append
+    if (index === this.length) {
+      this.append(value);
+      return this;
+    }
 
-      // Reach the node at that index
-      let newNode = new Node_2(value);
-      let previousNode = this.head;
+    // Reach the node at that index
+    let newNode = new Node_2(value);
+    let previousNode = this.head;
 
-      for (let k = 0; k < index - 1; k++) {
-          previousNode = previousNode.next;
-      }
+    for (let k = 0; k < index - 1; k++) {
+      previousNode = previousNode.next;
+    }
 
-      let nextNode = previousNode.next;
+    let nextNode = previousNode.next;
 
-      newNode.next = nextNode;
-      previousNode.next = newNode;
-      newNode.previous = previousNode;
-      nextNode.previous = newNode;
+    newNode.next = nextNode;
+    previousNode.next = newNode;
+    newNode.previous = previousNode;
+    nextNode.previous = newNode;
 
-      this.length++;
-      this.printList();
+    this.length++;
+    this.printList();
   }
 
   // Remove a node
   remove(index) {
-      if (!Number.isInteger(index) || index < 0 || index > this.length) {
-          console.log(`Invalid index. Current length is ${this.length}.`);
-          return this;
-      }
+    if (!Number.isInteger(index) || index < 0 || index > this.length) {
+      console.log(`Invalid index. Current length is ${this.length}.`);
+      return this;
+    }
 
-      // Remove head
-      if (index === 0) {
-          this.head = this.head.next;
-          this.head.previous = null;
-
-          this.length--;
-          this.printList();
-          return this;
-      }
-
-      // Remove tail
-      if (index === this.length - 1) {
-          this.tail = this.tail.previous;
-          this.tail.next = null;
-
-          this.length--;
-          this.printList();
-          return this;
-      }
-
-      // Remove node at an index
-      let previousNode = this.head;
-
-      for (let k = 0; k < index - 1; k++) {
-          previousNode = previousNode.next;
-      }
-      let deleteNode = previousNode.next;
-      let nextNode = deleteNode.next;
-
-      previousNode.next = nextNode;
-      nextNode.previous = previousNode;
+    // Remove head
+    if (index === 0) {
+      this.head = this.head.next;
+      this.head.previous = null;
 
       this.length--;
       this.printList();
       return this;
+    }
+
+    // Remove tail
+    if (index === this.length - 1) {
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+
+      this.length--;
+      this.printList();
+      return this;
+    }
+
+    // Remove node at an index
+    let previousNode = this.head;
+
+    for (let k = 0; k < index - 1; k++) {
+      previousNode = previousNode.next;
+    }
+    let deleteNode = previousNode.next;
+    let nextNode = deleteNode.next;
+
+    previousNode.next = nextNode;
+    nextNode.previous = previousNode;
+
+    this.length--;
+    this.printList();
+    return this;
   }
 }
