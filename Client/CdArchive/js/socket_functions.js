f5dc5794fc3a61e4c25628c695a38b0775d52dca
NeuralNuts <wilsonzakk8@gmail.com>
2023-05-12 13:22:50 +1000

    misc

--- Client/CdArchive/js/socket_functions.js
+++ Client/CdArchive/js/socket_functions.js
@@ -1,7 +1,7 @@
 // Connect to the server using socket.io
 const socket = io("http://localhost:3000");
-console.log(socket)
 window.$ = window.jQuery = require('jquery');
+
 var today = new Date();
 var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -9,33 +9,34 @@ var dateTime = date + time;
 
 // Listen for incoming messages from the server
 socket.on('message', (section_data) => {
-    //Add the message to the chat log
     var newMessage = `<input style="width: 100%;" id="pro-log-input"/>`;
     var chatLog = document.getElementById('log-div');
+    
     chatLog.innerHTML = newMessage
-    const sectionInput = document.getElementById('pro-log-input')
+
+    let sectionInput = document.getElementById('pro-log-input')
+
     sectionInput.value = section_data;
 
     socket.on('message_2', (barcode_data) => {
-        //Add the message to the chat log
-        const chatLog = document.getElementById('log-div');
-        const barCodeInput = document.getElementById("bar-code-input")
+        var barCodeInput = document.getElementById("bar-code-input")
         barCodeInput.value = barcode_data
 
         socket.on('message_x', (x_data) => {
-            //Add the message to the chat log
-            const chatLog = document.getElementById('log-div');
-            const newMessage = document.createElement('input');
+            var chatLog = document.getElementById('log-div');
+            var newMessage = document.createElement('input');
+            var xInput = document.getElementById("x-input")
+
             newMessage.value = x_data;
-            const xInput = document.getElementById("x-input")
             xInput.value = x_data
 
             socket.on('message_y', (y_data) => {
-                const yInput = document.getElementById("y-input")
-                yInput.value = y_data
+                var yInput = document.getElementById("y-input")
+                var myDoublyList;
 
+                yInput.value = y_data
                 results = `Date/Time: ${dateTime} | ${section_data} | ${barcode_data} | ${x_data} | ${y_data} | ADD-FROM-ROBOT`
-                var myDoublyList = new DoublyLinkedList(results);
+                myDoublyList = new DoublyLinkedList(results);
                 sectionInput.value = JSON.stringify(myDoublyList.printList().head.value)
                 chatLog.appendChild(sectionInput);
             });
@@ -52,53 +53,85 @@ window.addEventListener('load', () => {
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         message = "sort"
+
         var newMessage = `<input style="width: 100%;" id="pro-log-input"/>`;
         var chatLog_1 = document.getElementById('log-div');
+        var sectionInput = document.getElementById("section-input").value
+        var barCodeInput = document.getElementById("bar-code-input").value
+        var myDoublyList;
+
         chatLog_1.innerHTML = newMessage
-        const sectionInput = document.getElementById('pro-log-input')
-        results = `Date/Time: ${dateTime} | RETURN-SORTED-CD-ARCHIVE-TABLE-TO-ROBOT`
-        var myDoublyList = new DoublyLinkedList(results);
-        sectionInput.value = JSON.stringify(myDoublyList.printList().head.value)
-        chatLog_1.appendChild(sectionInput);
+
+        let logInput = document.getElementById('pro-log-input')
+
+        results = `Date/Time: ${dateTime} | Section: ${sectionInput} | Barcode: ${barCodeInput} | RETURN-SORTED-CD-ARCHIVE-TABLE-TO-ROBOT`
+        myDoublyList = new DoublyLinkedList(results);
+        logInput.value = JSON.stringify(myDoublyList.printList().head.value)
+        chatLog_1.appendChild(logInput);
+
         socket.emit('table', message);
     });
     form2.addEventListener('submit', (event) => {
         event.preventDefault();
+        message = "retrieve cd"
+
         var newMessage = `<input style="width: 100%;" id="pro-log-input"/>`;
         var chatLog_1 = document.getElementById('log-div');
+        var sectionInput = document.getElementById("section-input").value
+        var barCodeInput = document.getElementById("bar-code-input").value
+        var myDoublyList;
+
         chatLog_1.innerHTML = newMessage
-        const sectionInput = document.getElementById('pro-log-input')
-        results = `Date/Time: ${dateTime} RETRIEVE-CD-FROM-ROBOT`
-        var myDoublyList = new DoublyLinkedList(results);
-        sectionInput.value = JSON.stringify(myDoublyList.printList().head.value)
-        chatLog_1.appendChild(sectionInput);
-        message = "retrieve cd"
+
+        let logInput = document.getElementById('pro-log-input')
+
+        results = `Date/Time: ${dateTime} | Section: ${sectionInput} | Barcode: ${barCodeInput} | RETRIEVE-CD-FROM-ROBOT`
+        myDoublyList = new DoublyLinkedList(results);
+        logInput.value = JSON.stringify(myDoublyList.printList().head.value)
+        chatLog_1.appendChild(logInput);
+        
         socket.emit('table', message);
     });
     form3.addEventListener('submit', (event) => {
         event.preventDefault();
         message = "sort"
+
         var newMessage = `<input style="width: 100%;" id="pro-log-input"/>`;
         var chatLog_1 = document.getElementById('log-div');
+        var sectionInput = document.getElementById("section-input").value
+        var barCodeInput = document.getElementById("bar-code-input").value
+        var myDoublyList;
+
         chatLog_1.innerHTML = newMessage
-        const sectionInput = document.getElementById('pro-log-input')
-        results = `Date/Time: ${dateTime} REMOVE-CD-FROM-ROBOT`
-        var myDoublyList = new DoublyLinkedList(results);
-        sectionInput.value = JSON.stringify(myDoublyList.printList().head.value)
-        chatLog_1.appendChild(sectionInput);
+
+        let logInput = document.getElementById('pro-log-input')
+
+        results = `Date/Time: ${dateTime} | Section: ${sectionInput} | Barcode: ${barCodeInput} | REMOVE-CD-FROM-ROBOT`
+        myDoublyList = new DoublyLinkedList(results);
+        logInput.value = JSON.stringify(myDoublyList.printList().head.value)
+        chatLog_1.appendChild(logInput);
+
         socket.emit('table', message);
     });
     form4.addEventListener('submit', (event) => {
         event.preventDefault();
         message = "sort"
+
         var newMessage = `<input style="width: 100%;" id="pro-log-input"/>`;
         var chatLog_1 = document.getElementById('log-div');
+        var sectionInput = document.getElementById("section-input").value
+        var barCodeInput = document.getElementById("bar-code-input").value
+        var myDoublyList;
+
         chatLog_1.innerHTML = newMessage
-        const sectionInput = document.getElementById('pro-log-input')
-        results = `Date/Time: ${dateTime} ADD-CD-TO-ROBOT`
-        var myDoublyList = new DoublyLinkedList(results);
-        sectionInput.value = JSON.stringify(myDoublyList.printList().head.value)
-        chatLog_1.appendChild(sectionInput);
+
+        let logInput = document.getElementById('pro-log-input')
+
+        results = `Date/Time: ${dateTime} | Section: ${sectionInput} | Barcode: ${barCodeInput} | ADD-CD-TO-ROBOT`
+        myDoublyList = new DoublyLinkedList(results);
+        logInput.value = JSON.stringify(myDoublyList.printList().head.value)
+        chatLog_1.appendChild(logInput);
+
         socket.emit('table', message);
     });
 })
